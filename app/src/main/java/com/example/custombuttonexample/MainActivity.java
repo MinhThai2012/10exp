f6df55b2b752a76959bbd4c1b755d652d2782f15
MinhThai2012 <133180071+MinhThai2012@users.noreply.github.com>
2023-05-20 11:04:12 +0700

    first commit

---
+++ app/src/main/java/com/example/custombuttonexample/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.custombuttonexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstaceState)
+        super.onCreate(saveInstaceState)
+        setContentView(R.layout.activity_main)
+}
